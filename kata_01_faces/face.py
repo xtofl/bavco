e6c21e29c2ba733531a3a532fcd3a4dab5bea42f
xtofl <kristoffel.pirard@gmail.com>
2019-11-02 16:57:02 +0100

    add optional --nose

--- kata_01_faces/face.py
+++ kata_01_faces/face.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
-def face():
-    return """
+from argparse import ArgumentParser
+
+def face(options):
+    parts = ["""
     -------
-    | o o |
-    | --- |
+    | o o |"""]
+    if options.nose:
+        parts.append("    |  {}  |".format(options.nose))
+    parts.append("""    | --- |
     -------
-        """
+        """)
+    return "\n".join(parts)
 
 def main():
-    print(face())
+    parser = ArgumentParser()
+    parser.add_argument("--nose", default=None)
+    print(face(parser.parse_args()))
 
 if __name__ == "__main__":
     main()
